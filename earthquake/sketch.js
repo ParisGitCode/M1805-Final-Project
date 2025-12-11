3441e172cc6f8640903a7923c048f9662b4afd80
ParisGitCode <npofoegbu@outlook.com>
2025-12-10 19:19:59 -0600

    Add images for lights, soil, and concrete
    Added new images for visual elements and adjusted their placements.

--- earthquake/sketch.js
+++ earthquake/sketch.js
@@ -5,6 +5,10 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.cs
 let table;
 let bg;
 let cornerImg;
+let lightImg;
+let light2Img;
+let soil;
+let concrete;
 
 //  Here I am setting a table and background to place this data on the page. I also didn't know how to put the images in a folde so I've just kind of elft it sitting out.
 
@@ -16,6 +20,10 @@ function preload() {
   
   cornerImg = loadImage('earthquake/grass2.jpg'); //I'm unsure if this will work but the goal is to place this on the top left part of the screen. It's an image of the soil and grass.
  // The artistic viewpoint was to get a shaky image to kind of play the effect of an earthquake.
+  lightImg = loadImage('earthquake/lights.jpg');
+  light2Img = loadImage('earthquake/light2.jpg');
+  soil = loadImage('earthquake/soil.jpg');
+  concrete = loadImage('earthquake/concrete.jpg');
 }
 function setup() {
   createCanvas(640, 382); // Here I'm mmatching the image with the background size showing in the function.
@@ -23,10 +31,24 @@ function setup() {
   textSize(12);
 }
 
+image(cornerImg, 0, 0, 100, 100);
+
+// Top-right corner
+image(cornerImg, 640 - 100, 0, 100, 100);
+
+// Bottom-left corner
+image(cornerImg, 0, 382 - 100, 100, 100);
+
+// Bottom-right corner
+image(cornerImg, 640 - 100, 382 - 100, 100, 100);
+
 function draw() {
   background(bg);
-  image(cornerImg, 0, 0, 100, 100); // Here I'm trying to place the image in the top left.
-
+  image(cornerImg, 0, 0, 60, 60); // Here I'm trying to place the image in the top left.
+  image(lightImg, 640 - 100, 0, 50, 50);
+  image(light2Img,  0, 382 - 100, 30, 40);
+  image(soil, 640 - 100, 382 - 100, 46, 46); // I'm resizing the images so they're not too big, and I'm also placing them in corners. I want thhem to look a littlle messy for artistic purposes. I want everything to look rather abstract and chaotic.
+  
   // Here I'm making the timer change as the project progresses.
   if (frameCount % 60 === 0) timer++;
   fill(255);
